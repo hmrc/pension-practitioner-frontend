ae72f71dc50f7a6f9c969d10ec80b9c2854f4a31
Suryabala Singh <22298166+suryabalasingh@users.noreply.github.com>
2020-07-09 12:12:28 +0100

    PODS-4449-Added the page

---
+++ test/controllers/individual/UseAddressForContactControllerSpec.scala
@@ -0,0 +1,185 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.individual
+
+import controllers.base.ControllerSpecBase
+import forms.individual.UseAddressForContactFormProvider
+import matchers.JsonMatchers
+import models.{NormalMode, TolerantAddress, UserAnswers}
+import org.mockito.ArgumentCaptor
+import org.mockito.Matchers.{any, eq => eqTo}
+import org.mockito.Mockito.{times, verify, when}
+import org.scalatest.{OptionValues, TryValues}
+import org.scalatestplus.mockito.MockitoSugar
+import pages.individual.{IndividualAddressPage, UseAddressForContactPage}
+import play.api.data.Form
+import play.api.inject.bind
+import play.api.libs.json.{JsObject, Json}
+import play.api.mvc.Call
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import play.twirl.api.Html
+import uk.gov.hmrc.viewmodels.{NunjucksSupport, Radios}
+import utils.countryOptions.CountryOptions
+
+import scala.concurrent.Future
+
+class UseAddressForContactControllerSpec extends ControllerSpecBase with MockitoSugar with NunjucksSupport with JsonMatchers with OptionValues with TryValues {
+
+  private def onwardRoute = Call("GET", "/foo")
+
+  private val formProvider = new UseAddressForContactFormProvider()
+  private val form = formProvider()
+
+  private def useAddressForContactGetRoute: String = routes.UseAddressForContactController.onPageLoad(NormalMode).url
+
+  private def useAddressForContactPostRoute: String = routes.UseAddressForContactController.onSubmit().url
+
+  private val address = TolerantAddress(Some("line1"), Some("line2"), Some("line3"), Some("line4"), Some("post code"), Some("GB"))
+  private val countryOptions: CountryOptions = mock[CountryOptions]
+  private val templateCaptor = ArgumentCaptor.forClass(classOf[String])
+  private val jsonCaptor = ArgumentCaptor.forClass(classOf[JsObject])
+  private val uaWithIndividualAddress = UserAnswers().set(IndividualAddressPage, address).success.value
+
+  private def expectedJson(form: Form[Boolean]): JsObject = Json.obj(
+    "form" -> form,
+    "submitUrl" -> useAddressForContactPostRoute,
+    "radios" -> Radios.yesNo(form("value")),
+    "address" -> address.lines(countryOptions)
+  )
+
+  override def beforeEach: Unit = {
+    super.beforeEach
+    when(countryOptions.getCountryNameFromCode(eqTo(address))).thenReturn(Some("United Kingdom"))
+    when(mockRenderer.render(any(), any())(any())).thenReturn(Future.successful(Html("")))
+  }
+
+  "UseAddressForContactController" when {
+
+    "on a GET" must {
+
+      "return OK with the correct view" in {
+        val application = applicationBuilder(userAnswers = Some(uaWithIndividualAddress),
+          extraModules = Seq(bind[CountryOptions].toInstance(countryOptions))).overrides().build()
+        val request = FakeRequest(GET, useAddressForContactGetRoute)
+        val result = route(application, request).value
+
+        status(result) mustEqual OK
+
+        verify(mockRenderer, times(1)).render(templateCaptor.capture(), jsonCaptor.capture())(any())
+
+        templateCaptor.getValue mustEqual "individual/useAddressForContact.njk"
+        jsonCaptor.getValue must containJson(expectedJson(form))
+
+        application.stop()
+      }
+
+      "return OK and populate the view correctly when the question has previously been answered" in {
+        val application = applicationBuilder(userAnswers = Some(uaWithIndividualAddress.set(UseAddressForContactPage, true).success.value),
+          extraModules = Seq(bind[CountryOptions].toInstance(countryOptions))).overrides().build()
+        val request = FakeRequest(GET, useAddressForContactGetRoute)
+
+        val result = route(application, request).value
+
+        status(result) mustEqual OK
+
+        verify(mockRenderer, times(1)).render(templateCaptor.capture(), jsonCaptor.capture())(any())
+
+        val filledForm = form.bind(Map("value" -> "true"))
+
+        templateCaptor.getValue mustEqual "individual/useAddressForContact.njk"
+        jsonCaptor.getValue must containJson(expectedJson(filledForm))
+
+        application.stop()
+      }
+
+      "redirect to Session Expired if no existing data is found" in {
+
+        val application = applicationBuilder(userAnswers = None).build()
+
+        val request = FakeRequest(GET, useAddressForContactGetRoute)
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+
+        redirectLocation(result).value mustEqual controllers.routes.SessionExpiredController.onPageLoad().url
+
+        application.stop()
+      }
+    }
+
+    "on a POST" must {
+
+      "redirect to the next page when valid data is submitted" in {
+        when(mockUserAnswersCacheConnector.save(any())(any(), any())) thenReturn Future.successful(Json.obj())
+        when(mockCompoundNavigator.nextPage(any(), any(), any())).thenReturn(onwardRoute)
+
+        val application = applicationBuilder(userAnswers = Some(UserAnswers()),
+          extraModules = Seq(bind[CountryOptions].toInstance(countryOptions))).overrides().build()
+
+        val request = FakeRequest(POST, useAddressForContactPostRoute)
+          .withFormUrlEncodedBody(("value", "true"))
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+
+        redirectLocation(result).value mustEqual onwardRoute.url
+
+        application.stop()
+      }
+    }
+
+    "return a Bad Request and errors when invalid data is submitted" in {
+      val application = applicationBuilder(userAnswers = Some(uaWithIndividualAddress),
+        extraModules = Seq(bind[CountryOptions].toInstance(countryOptions))).overrides().build()
+
+      val request = FakeRequest(POST, useAddressForContactPostRoute).withFormUrlEncodedBody(("value", ""))
+      val boundForm = form.bind(Map("value" -> ""))
+      val result = route(application, request).value
+
+      status(result) mustEqual BAD_REQUEST
+
+      verify(mockRenderer, times(1)).render(templateCaptor.capture(), jsonCaptor.capture())(any())
+
+      templateCaptor.getValue mustEqual "individual/useAddressForContact.njk"
+      jsonCaptor.getValue must containJson(expectedJson(boundForm))
+
+      application.stop()
+    }
+
+    "redirect to Session Expired if no existing data is found" in {
+
+      val application = applicationBuilder(userAnswers = None).build()
+
+      val request =
+        FakeRequest(POST, useAddressForContactPostRoute)
+          .withFormUrlEncodedBody(("value", "true"))
+
+      val result = route(application, request).value
+
+      status(result) mustEqual SEE_OTHER
+
+      redirectLocation(result).value mustEqual controllers.routes.SessionExpiredController.onPageLoad().url
+
+      application.stop()
+    }
+  }
+}
+
+
