78fa08ccbda6df24f42032c5c01a1ac45a9353fa
Grant Robson <grant.robson@digital.hmrc.gov.uk>
2020-07-15 13:49:31 +0100

    PODS-4488 WIP

---
+++ test/controllers/register/BusinessDetailsNotFoundControllerSpec.scala
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.register
+
+import controllers.base.ControllerSpecBase
+import data.SampleData
+import org.mockito.ArgumentCaptor
+import org.mockito.Matchers.any
+import org.mockito.Mockito.times
+import org.mockito.Mockito.verify
+import org.mockito.Mockito.when
+import org.scalatestplus.mockito.MockitoSugar
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import play.twirl.api.Html
+
+import scala.concurrent.Future
+
+class BusinessDetailsNotFoundControllerSpec extends ControllerSpecBase with MockitoSugar {
+
+  "BusinessDetailsNotFound Controller" must {
+
+    "return OK and the correct view for a GET" in {
+
+      when(mockRenderer.render(any(), any())(any()))
+        .thenReturn(Future.successful(Html("")))
+
+      val application = applicationBuilder(userAnswers = Some(SampleData.emptyUserAnswers)).build()
+      val request = FakeRequest(GET, routes.BusinessDetailsNotFoundController.onPageLoad().url)
+      val templateCaptor = ArgumentCaptor.forClass(classOf[String])
+
+      val result = route(application, request).value
+
+      status(result) mustEqual OK
+
+      verify(mockRenderer, times(1)).render(templateCaptor.capture(), any())(any())
+
+      templateCaptor.getValue mustEqual "register/businessDetailsNotFound.njk"
+
+      application.stop()
+    }
+  }
+}
