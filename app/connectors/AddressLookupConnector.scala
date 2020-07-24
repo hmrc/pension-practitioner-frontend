c8aebbdbf165fae2df36f8778d987322d5c4eb50
Vincent Vinnicombe <5462800+vinnicombe@users.noreply.github.com>
2020-07-24 14:26:20 +0100

    PODS-4538: Verbs upgrade complete, tests fixed and warnings removed

--- app/connectors/AddressLookupConnector.scala
+++ app/connectors/AddressLookupConnector.scala
@@ -26,7 +26,7 @@ import play.api.Logger
 import play.api.http.Status._
 import play.api.libs.json.Reads
 import uk.gov.hmrc.play.bootstrap.http.HttpClient
-
+import uk.gov.hmrc.http.HttpReads.Implicits._
 import scala.concurrent.ExecutionContext
 import scala.concurrent.Future
 
@@ -50,7 +50,7 @@ class AddressLookupConnector @Inject()(http: HttpClient, config: FrontendAppConf
   }
 
   private def logExceptions: PartialFunction[Throwable, Future[Seq[TolerantAddress]]] = {
-    case (t: Throwable) => {
+    case t: Throwable => {
       Logger.error("Exception in AddressLookup", t)
       Future.failed(t)
     }
