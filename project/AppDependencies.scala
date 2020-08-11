ea6d544fcefcdb2451b44e618489e822e2c8901d
Grant Robson <grant.robson@digital.hmrc.gov.uk>
2020-08-11 08:54:44 +0100

    Change mongo library to fix QA deployment

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,7 +8,7 @@ object AppDependencies {
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
-    "org.reactivemongo"             %%  "play2-reactivemongo"            % "0.20.3-play26",
+    "uk.gov.hmrc"                   %%  "simple-reactivemongo"           % "7.23.0-play-26",
     "uk.gov.hmrc"                   %%  "logback-json-logger"            % "4.8.0",
     "uk.gov.hmrc"                   %%  "play-health"                    % "3.14.0-play-26",
     "uk.gov.hmrc"                   %%  "play-conditional-form-mapping"  % "1.3.0-play-26",
