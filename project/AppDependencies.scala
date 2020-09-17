6477159c639b9d299afe7465c8872631daf5075b
Grant Robson <grant.robson@digital.hmrc.gov.uk>
2020-09-17 11:46:03 +0100

    PODS-4666 update libs

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,7 +8,7 @@ object AppDependencies {
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"                   %%  "simple-reactivemongo"           % "7.23.0-play-26",
+    "uk.gov.hmrc"                   %%  "simple-reactivemongo"           % "7.30.0-play-26",
     "uk.gov.hmrc"                   %%  "logback-json-logger"            % "4.8.0",
     "uk.gov.hmrc"                   %%  "play-health"                    % "3.14.0-play-26",
     "uk.gov.hmrc"                   %%  "play-conditional-form-mapping"  % "1.3.0-play-26",
@@ -19,7 +19,7 @@ object AppDependencies {
     "org.webjars.npm"               %   "govuk-frontend"                 % "3.7.0",
     "org.webjars.npm"               %   "hmrc-frontend"                  % "1.15.1",
     "com.google.inject.extensions"  %   "guice-multibindings"            % "4.2.2",
-    "uk.gov.hmrc"                   %%  "domain"                         % "5.9.0-play-26"
+    "uk.gov.hmrc"                   %%  "domain"                         % "5.10.0-play-26"
   )
 
   val test = Seq(
