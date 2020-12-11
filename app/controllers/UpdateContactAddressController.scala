3714804c2104bc6d07a79e009f85dc47a2751a4a
Emma Rawsthorne <27211889+emrawsy@users.noreply.github.com>
2020-12-11 13:20:37 +0000

    PODS-4944 initial creation of new interrupt page

---
+++ app/controllers/UpdateContactAddressController.scala
@@ -0,0 +1,120 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import connectors.SubscriptionConnector
+import controllers.actions.{AuthAction, DataRequiredAction, DataRetrievalAction}
+import javax.inject.Inject
+import models.register.BusinessRegistrationType.Company
+import models.register.RegistrationLegalStatus.{Individual, LimitedCompany, Partnership}
+import models.{NormalMode, UserAnswers}
+import pages.RegistrationDetailsPage
+import pages.individual.IndividualDetailsPage
+import pages.partnership.BusinessNamePage
+import play.api.i18n.I18nSupport
+import play.api.libs.json.{JsObject, Json}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import renderer.Renderer
+import uk.gov.hmrc.play.bootstrap.controller.FrontendBaseController
+import utils.countryOptions.CountryOptions
+
+import scala.concurrent.{ExecutionContext, Future}
+import scala.reflect.runtime.universe.Throw
+
+class UpdateContactAddressController @Inject()(
+                                                val controllerComponents: MessagesControllerComponents,
+                                                renderer: Renderer,
+                                                authenticate: AuthAction,
+                                                getData: DataRetrievalAction,
+                                                requireData: DataRequiredAction,
+                                                //                                                countryOptions: CountryOptions,
+                                                subscriptionConnector: SubscriptionConnector
+                                              )(implicit ec: ExecutionContext)
+  extends FrontendBaseController
+    with I18nSupport {
+
+  def onPageLoad: Action[AnyContent] = (authenticate andThen getData andThen requireData).async {
+    implicit request =>
+      request.user.alreadyEnrolledPspId.map { pspId =>
+        subscriptionConnector.getSubscriptionDetails(pspId).flatMap { uaJson =>
+          val ua = UserAnswers(uaJson.as[JsObject])
+          val continueJson = continueUrl(ua) match {
+            case Some(url) => Json.obj("addressUrl" -> url)
+            case None => Json.obj()
+          }
+          val json = Json.obj(
+            //            "address" -> address.lines(countryOptions),
+          ) ++ continueJson
+          renderer.render("updateContactAddress.njk", json).map(Ok(_))
+        }
+      }.getOrElse(
+        Future.successful(Redirect(controllers.routes.SessionExpiredController.onPageLoad()))
+      )
+  }
+
+  private def continueUrl(ua: UserAnswers): Option[String] = {
+    ua.get(RegistrationDetailsPage).flatMap {
+      regInfo =>
+        regInfo.legalStatus match {
+          case LimitedCompany => Some(controllers.company.routes.CompanyPostcodeController.onPageLoad(NormalMode).url)
+          case Individual => Some(controllers.individual.routes.IndividualPostcodeController.onPageLoad(NormalMode).url)
+          case Partnership => Some(controllers.partnership.routes.PartnershipPostcodeController.onPageLoad(NormalMode).url)
+          case _ => None
+        }
+    }
+    //    ua.get(RegistrationDetailsPage).map { regInfo =>
+    //
+    //      regInfo.legalStatus match {
+    //        case Individual =>
+    //          val title: String = individualMessage("viewDetails.title").resolve
+    //          Json.obj(
+    //            "pageTitle" -> title,
+    //            "heading" -> ua.get(IndividualDetailsPage).fold(title)(name => heading(name.fullName)),
+    //            "list" -> individualDetails(ua, pspId),
+    //            "nextPage" -> nextPage,
+    //            "returnUrl" -> appConfig.returnToPspDashboardUrl,
+    //            "returnLink" -> ua.get(IndividualDetailsPage)
+    //              .fold(messages("site.return_to_dashboard"))(name => messages("site.return_to", name.fullName))
+    //          )
+    //        case LimitedCompany =>
+    //          val title: String = companyMessage("viewDetails.title").resolve
+    //          Json.obj(
+    //            "pageTitle" -> title,
+    //            "heading" -> ua.get(comp.BusinessNamePage).fold(title)(name => heading(name)),
+    //            "list" -> companyDetails(ua, pspId),
+    //            "nextPage" -> nextPage,
+    //            "returnUrl" -> appConfig.returnToPspDashboardUrl,
+    //            "returnLink" -> ua.get(comp.BusinessNamePage)
+    //              .fold(messages("site.return_to_dashboard"))(name => messages("site.return_to", name))
+    //          )
+    //        case Partnership =>
+    //          val title: String = partnershipMessage("viewDetails.title").resolve
+    //          Json.obj(
+    //            "pageTitle" -> title,
+    //            "heading" -> ua.get(BusinessNamePage).fold(title)(name => heading(name)),
+    //            "list" -> partnershipDetails(ua, pspId),
+    //            "nextPage" -> nextPage,
+    //            "returnUrl" -> appConfig.returnToPspDashboardUrl,
+    //            "returnLink" -> ua.get(BusinessNamePage)
+    //              .fold(messages("site.return_to_dashboard"))(name => messages("site.return_to", name))
+    //          )
+    //      }
+    //    }.getOrElse(Json.obj())
+  }
+
+
+}
---
+++ test/controllers/UpdateContactAddressControllerSpec.scala
@@ -0,0 +1,141 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import config.FrontendAppConfig
+import connectors.SubscriptionConnector
+import connectors.cache.UserAnswersCacheConnector
+import controllers.actions.{AuthAction, DataRequiredAction, DataRequiredActionImpl, FakeAuthAction}
+import controllers.base.ControllerSpecBase
+import data.SampleData._
+import models.{NormalMode, UserAnswers}
+import navigators.CompoundNavigator
+import org.mockito.ArgumentCaptor
+import org.mockito.Matchers.any
+import org.mockito.Mockito.{times, verify, when}
+import org.scalatestplus.mockito.MockitoSugar
+import play.api.inject.bind
+import play.api.inject.guice.GuiceableModule
+import play.api.libs.json.{JsNull, JsObject}
+import play.api.mvc.Call
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import play.twirl.api.Html
+import uk.gov.hmrc.nunjucks.NunjucksRenderer
+import services.PspDetailsHelper._
+
+import scala.concurrent.Future
+
+class UpdateContactAddressControllerSpec extends ControllerSpecBase with MockitoSugar {
+  private def onwardRoute = Call("GET", "/foo")
+  private val mockSubscriptionConnector = mock[SubscriptionConnector]
+
+  override def modules: Seq[GuiceableModule] = Seq(
+    bind[SubscriptionConnector].toInstance(mockSubscriptionConnector),
+    bind[DataRequiredAction].to[DataRequiredActionImpl],
+    bind[AuthAction].to[FakeAuthAction],
+    bind[NunjucksRenderer].toInstance(mockRenderer),
+    bind[FrontendAppConfig].toInstance(mockAppConfig),
+    bind[UserAnswersCacheConnector].toInstance(mockUserAnswersCacheConnector),
+    bind[CompoundNavigator].toInstance(mockCompoundNavigator)
+  )
+
+  "UpdateContactAddress Controller" must {
+
+    "return OK and the correct view for a GET on company" in {
+      val ua = UserAnswers()
+
+      when(mockRenderer.render(any(), any())(any()))
+        .thenReturn(Future.successful(Html("")))
+      when(mockCompoundNavigator.nextPage(any(), any(), any())).thenReturn(onwardRoute)
+      when(mockSubscriptionConnector.getSubscriptionDetails(any())(any(), any())).thenReturn(Future.successful(uaCompanyUk))
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+      val request = FakeRequest(GET, routes.UpdateContactAddressController.onPageLoad().url)
+      val templateCaptor = ArgumentCaptor.forClass(classOf[String])
+      val jsonCaptor = ArgumentCaptor.forClass(classOf[JsObject])
+
+      val result = route(application, request).value
+
+      status(result) mustEqual OK
+
+      verify(mockRenderer, times(1)).render(templateCaptor.capture(), jsonCaptor.capture())(any())
+
+      templateCaptor.getValue mustEqual "updateContactAddress.njk"
+
+      (jsonCaptor.getValue \ "addressUrl").asOpt[String]mustEqual
+        Some(controllers.company.routes.CompanyPostcodeController.onPageLoad(NormalMode).url)
+
+
+      application.stop()
+    }
+
+    "return OK and the correct view for a GET on individual" in {
+
+      when(mockRenderer.render(any(), any())(any()))
+        .thenReturn(Future.successful(Html("")))
+      when(mockCompoundNavigator.nextPage(any(), any(), any())).thenReturn(onwardRoute)
+      when(mockSubscriptionConnector.getSubscriptionDetails(any())(any(), any())).thenReturn(Future.successful(uaIndividualUK))
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+      val request = FakeRequest(GET, routes.UpdateContactAddressController.onPageLoad().url)
+      val templateCaptor = ArgumentCaptor.forClass(classOf[String])
+      val jsonCaptor = ArgumentCaptor.forClass(classOf[JsObject])
+
+      val result = route(application, request).value
+
+      status(result) mustEqual OK
+
+      verify(mockRenderer, times(1)).render(templateCaptor.capture(), jsonCaptor.capture())(any())
+
+      templateCaptor.getValue mustEqual "updateContactAddress.njk"
+
+      (jsonCaptor.getValue \ "addressUrl").asOpt[String]mustEqual
+        Some(controllers.individual.routes.IndividualPostcodeController.onPageLoad(NormalMode).url)
+
+
+      application.stop()
+    }
+
+    "return OK and the correct view for a GET on partnership" in {
+
+      when(mockRenderer.render(any(), any())(any()))
+        .thenReturn(Future.successful(Html("")))
+      when(mockCompoundNavigator.nextPage(any(), any(), any())).thenReturn(onwardRoute)
+      when(mockSubscriptionConnector.getSubscriptionDetails(any())(any(), any())).thenReturn(Future.successful(uaPartnershipNonUK))
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+      val request = FakeRequest(GET, routes.UpdateContactAddressController.onPageLoad().url)
+      val templateCaptor = ArgumentCaptor.forClass(classOf[String])
+      val jsonCaptor = ArgumentCaptor.forClass(classOf[JsObject])
+
+      val result = route(application, request).value
+
+      status(result) mustEqual OK
+
+      verify(mockRenderer, times(1)).render(templateCaptor.capture(), jsonCaptor.capture())(any())
+
+      templateCaptor.getValue mustEqual "updateContactAddress.njk"
+
+      (jsonCaptor.getValue \ "addressUrl").asOpt[String]mustEqual
+        Some(controllers.partnership.routes.PartnershipPostcodeController.onPageLoad(NormalMode).url)
+
+
+      application.stop()
+    }
+  }
+}
