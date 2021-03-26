0f34f49583eaad86ff142b4ffb3feee13481c0cd
Vincent Vinnicombe <5462800+vinnicombe@users.noreply.github.com>
2021-03-26 07:42:20 +0000

    libraries: Updated libs, added nonce to tracking snippet macro

--- app/renderer/Renderer.scala
+++ app/renderer/Renderer.scala
@@ -17,27 +17,36 @@
 package renderer
 
 import config.FrontendAppConfig
+
 import javax.inject.Inject
-import play.api.libs.json.{Json, OWrites, JsObject}
+import play.api.libs.json.{JsObject, JsString, Json, OWrites}
 import play.api.mvc.RequestHeader
 import play.twirl.api.Html
 import uk.gov.hmrc.nunjucks.NunjucksRenderer
+import views.html.helper.CSPNonce
 
 import scala.concurrent.Future
 
 class Renderer @Inject()(appConfig: FrontendAppConfig, renderer: NunjucksRenderer) {
 
-  def render(template: String)(implicit request: RequestHeader): Future[Html] =
+  def render(template: String)
+            (implicit request: RequestHeader): Future[Html] =
     renderTemplate(template, Json.obj())
 
-  def render[A](template: String, ctx: A)(implicit request: RequestHeader, writes: OWrites[A]): Future[Html] =
+  def render[A](template: String, ctx: A)
+               (implicit request: RequestHeader, writes: OWrites[A]): Future[Html] =
     renderTemplate(template, Json.toJsObject(ctx))
 
-  def render(template: String, ctx: JsObject)(implicit request: RequestHeader): Future[Html] =
+  def render(template: String, ctx: JsObject)
+            (implicit request: RequestHeader): Future[Html] =
     renderTemplate(template, ctx)
 
-  private def renderTemplate(template: String, ctx: JsObject)(implicit request: RequestHeader): Future[Html] =
-    renderer.render(template, ctx ++ Json.obj("config" -> config))
+  private def renderTemplate(template: String, ctx: JsObject)
+                            (implicit request: RequestHeader): Future[Html] =
+    renderer.render(
+      template = template,
+      ctx = ctx ++ Json.obj("config" -> config.+("nonce" -> JsString(CSPNonce.get.getOrElse(""))))
+    )
 
   private lazy val config: JsObject = Json.obj(
     "betaFeedbackUnauthenticatedUrl"  -> appConfig.betaFeedbackUnauthenticatedUrl,
