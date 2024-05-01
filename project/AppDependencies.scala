1aec4f2356890588d010dd31fe29f9b50c600dd5
LauraToddCodes <74312582+LauraToddCodes@users.noreply.github.com>
2024-05-01 12:42:26 +0100

    PODS-9317 - Upgrade dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -11,18 +11,18 @@ object AppDependencies {
     "org.webjars.npm"               %  "govuk-frontend"                         % "4.8.0",
     "com.google.inject.extensions"  %  "guice-multibindings"                    % "4.2.3",
     "uk.gov.hmrc"                   %% "domain-play-30"                         % "9.0.0",
-    "com.fasterxml.jackson.module"  %% "jackson-module-scala"                   % "2.16.1"
+    "com.fasterxml.jackson.module"  %% "jackson-module-scala"                   % "2.17.0"
   )
 
   val test = Seq(
     "uk.gov.hmrc"                 %% "bootstrap-test-play-30"  % bootstrapVersion,
-    "org.scalatest"               %% "scalatest"               % "3.2.15",
-    "org.scalatestplus.play"      %% "scalatestplus-play"      % "5.1.0",
+    "org.scalatest"               %% "scalatest"               % "3.2.18",
+    "org.scalatestplus.play"      %% "scalatestplus-play"      % "7.0.1",
     "org.scalatestplus"           %% "mockito-4-6"             % "3.2.15.0",
-    "org.scalatestplus"           %% "scalacheck-1-17"         % "3.2.15.0",
+    "org.scalatestplus"           %% "scalacheck-1-17"         % "3.2.18.0",
     "org.pegdown"                 %  "pegdown"                 % "1.6.0",
-    "org.scalacheck"              %% "scalacheck"              % "1.17.0",
-    "org.jsoup"                   %  "jsoup"                   % "1.15.4",
+    "org.scalacheck"              %% "scalacheck"              % "1.18.0",
+    "org.jsoup"                   %  "jsoup"                   % "1.17.2",
     "org.mockito"                 %% "mockito-scala"           % "1.17.31",
     "io.github.wolfendale"        %% "scalacheck-gen-regexp"   % "1.1.0",
     "com.vladsch.flexmark"        %  "flexmark-all"            % "0.64.8"
