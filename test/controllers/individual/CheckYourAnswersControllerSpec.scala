31a6ed3e4e48c98f3c8ae2b20bc5eb4943afe444
Veera Kota <56839211+dynamicwave4@users.noreply.github.com>
2024-09-05 15:45:17 +0100

    PODS-9742: fixed unit test cases in individual folder (#213)

--- test/controllers/individual/CheckYourAnswersControllerSpec.scala
+++ test/controllers/individual/CheckYourAnswersControllerSpec.scala
@@ -20,20 +20,21 @@ import controllers.actions.MutableFakeDataRetrievalAction
 import controllers.base.ControllerSpecBase
 import matchers.JsonMatchers
 import models.UserAnswers
-import org.mockito.ArgumentCaptor
 import org.mockito.ArgumentMatchers.any
 import org.mockito.Mockito._
 import org.scalatest.{OptionValues, TryValues}
 import org.scalatestplus.mockito.MockitoSugar
 import play.api.Application
 import play.api.inject.bind
-import play.api.libs.json.{JsObject, Json}
+import play.api.mvc.Call
 import play.api.test.Helpers._
 import play.twirl.api.Html
 import services.IndividualCYAService
 import uk.gov.hmrc.viewmodels.NunjucksSupport
 import uk.gov.hmrc.viewmodels.SummaryList.{Key, Row, Value}
 import uk.gov.hmrc.viewmodels.Text.Literal
+import utils.TwirlMigration
+import views.html.CheckYourAnswersView
 
 import scala.concurrent.Future
 
@@ -42,21 +43,18 @@ class CheckYourAnswersControllerSpec extends ControllerSpecBase with MockitoSuga
 
   private val mutableFakeDataRetrievalAction: MutableFakeDataRetrievalAction = new MutableFakeDataRetrievalAction()
   private val individualCYAService = mock[IndividualCYAService]
-  private val application: Application =
+  override def fakeApplication(): Application =
     applicationBuilderMutableRetrievalAction(
       mutableFakeDataRetrievalAction,
       extraModules = Seq(bind[IndividualCYAService].toInstance(individualCYAService))).build()
-  private val templateToBeRendered = "check-your-answers.njk"
 
   private def onPageLoadUrl: String = routes.CheckYourAnswersController.onPageLoad().url
-  private def redirectUrl: String = controllers.individual.routes.DeclarationController.onPageLoad().url
+  private def redirectUrl: Call = controllers.individual.routes.DeclarationController.onPageLoad()
 
   private val list: Seq[Row] = Seq(Row(
     key = Key(msg"cya.name", classes = Seq("govuk-!-width-one-half")),
     value = Value(Literal("first last"), classes = Seq("govuk-!-width-one-third"))
   ))
-
-  private val jsonToPassToTemplate: JsObject = Json.obj("list" -> list, "redirectUrl" -> redirectUrl)
 
   override def beforeEach(): Unit = {
     super.beforeEach()
@@ -66,25 +64,23 @@ class CheckYourAnswersControllerSpec extends ControllerSpecBase with MockitoSuga
 
   "CheckYourAnswers Controller" must {
     "return OK and the correct view for a GET" in {
-      val templateCaptor = ArgumentCaptor.forClass(classOf[String])
-      val jsonCaptor = ArgumentCaptor.forClass(classOf[JsObject])
-
+      val request = httpGETRequest(onPageLoadUrl)
       when(individualCYAService.individualCya(any())(any())).thenReturn(list)
 
-      val result = route(application, httpGETRequest(onPageLoadUrl)).value
+      val view = app.injector.instanceOf[CheckYourAnswersView].apply(redirectUrl,
+        TwirlMigration.summaryListRow(list))(request, messages)
+
+      val result = route(app, request).value
 
       status(result) mustEqual OK
-
-      verify(mockRenderer, times(1)).render(templateCaptor.capture(), jsonCaptor.capture())(any())
-
-      templateCaptor.getValue mustEqual templateToBeRendered
-      jsonCaptor.getValue must containJson(jsonToPassToTemplate)
+      compareResultAndView(result, view)
     }
 
     "redirect to Session Expired page for a GET when there is no data" in {
+      val request = httpGETRequest(onPageLoadUrl)
       mutableFakeDataRetrievalAction.setDataToReturn(None)
 
-      val result = route(application, httpGETRequest(onPageLoadUrl)).value
+      val result = route(app, request).value
 
       status(result) mustEqual SEE_OTHER
 
